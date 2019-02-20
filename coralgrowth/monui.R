8477fac8483c1e17a60bc095af97d794e37f2fb7
Jack177 <jordan.benrezkallah@gmail.com>
2019-02-20 15:54:24 +0100

    Brouillon graphique : croissance des coraux

---
+++ coralgrowth/monui.R
@@ -0,0 +1,19 @@
+library(shiny)
+
+#Definie le UI, fluidpage modifie la taille des fenetres
+shinyUI(
+  fluidPage(
+    titlePanel("Evolution de la croissance des coraux"),
+
+  #Sidebar
+  sidebarLayout(
+    sidebarPanel(
+      radioButtons(inputId = "Coul", label = "Couleur du graphique",
+                   choices = c("Bleu claire" = "lightblue", "Gris" = "gray",
+                               selected = "lightblue"))),
+    mainPanel(plotOutput("monplot"))
+    )
+  )
+)
+
+
